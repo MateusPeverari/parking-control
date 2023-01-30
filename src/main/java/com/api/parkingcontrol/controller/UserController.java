51e4a5f40808282a035c5d7f20002bb933890d6e
Mateus Campos Peverari <mateus.peverari@gmail.com>
2023-01-30 17:54:13 -0300

    feat: creating User GET Methods

--- src/main/java/com/api/parkingcontrol/controller/UserController.java
+++ src/main/java/com/api/parkingcontrol/controller/UserController.java
@@ -6,10 +6,14 @@ import com.api.parkingcontrol.services.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -21,6 +25,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<Object> saveUser(@RequestBody @Valid UserDto userDto) {
         var userModel = new UserModel();
@@ -29,5 +34,21 @@ public class UserController {
         String passwordEncode = bCryptPasswordEncoder.encode(userModel.getPassword());
         userModel.setPassword(passwordEncode);
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userModel));
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
+    @GetMapping
+    public ResponseEntity<List<UserModel>> getAllUsers() {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getUserById(@PathVariable(value = "id") UUID id) {
+        Optional<UserModel> userModelOptional = userService.findById(id);
+        if (userModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(userModelOptional.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
     }
 }
