de2b5b7ebcac63635c2c88bc562ff27ccbb0faba
Mateus Campos Peverari <mateus.peverari@gmail.com>
2023-01-30 16:56:05 -0300

    feat: creating 'UserService'

--- src/main/java/com/api/parkingcontrol/services/UserService.java
+++ src/main/java/com/api/parkingcontrol/services/UserService.java
@@ -1,2 +1,36 @@
-package com.api.parkingcontrol.services;public class UserService {
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.models.UserModel;
+import com.api.parkingcontrol.repositories.UserRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class UserService {
+    final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public UserModel save(UserModel userModel) {
+        return userRepository.save(userModel);
+    }
+
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<UserModel> findById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    @Transactional
+    public void delete(UserModel userModel) {
+        userRepository.delete(userModel);
+    }
+
 }
