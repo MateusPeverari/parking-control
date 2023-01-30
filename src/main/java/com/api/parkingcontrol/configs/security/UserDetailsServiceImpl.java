d2ec75e661f8a07689f8266241c62d052cb6b66c
Mateus Campos Peverari <mateus.peverari@gmail.com>
2023-01-30 15:18:07 -0300

    feat: adding user roles

--- src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
+++ src/main/java/com/api/parkingcontrol/configs/security/UserDetailsServiceImpl.java
@@ -2,12 +2,16 @@ package com.api.parkingcontrol.configs.security;
 
 import com.api.parkingcontrol.models.UserModel;
 import com.api.parkingcontrol.repositories.UserRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     final UserRepository userRepository;
@@ -18,7 +22,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username)
+        UserModel userModel = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+        return new User(userModel.getUsername(), userModel.getPassword(), true, true, true,true, userModel.getAuthorities());
     }
 }
